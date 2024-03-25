e50ab8b672c8b3452bb5daacb19daf9f331e6f35
pjm <2556323541@qq.com>
2024-03-26 00:40:34 +0800

    add example06: zset rank

---
+++ go-redis-example/example/ex06_ranking.go
@@ -0,0 +1,79 @@
+package example
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+const ex06RankKey = "ex06_rank_zset"
+
+type ex06ItemScore struct {
+	ItemName string
+	Score    float64
+}
+
+// Ex06 排行榜
+// go run main.go init // 初始化积分
+// go run main.go Ex06 rev_order // 输出完整榜单
+// go run main.go Ex06 order_page 0 // 逆序分页输出，offset=1
+// go run main.go Ex06 get_rank user2 // 获取user2的排名
+// go run main.go Ex06 get_score user2 // 获取user2的分数
+// go run main.go Ex06 add_user_score user2 10 // 为user2设置为10分
+// zadd ex06_rank_zset 15 andy
+// zincrby ex06_rank_zset -9 andy // andy 扣9分，排名掉到最后一名
+
+func ex06GetOrderListByPage(ctx context.Context, offset, pageSize int64) {
+	// zrange ex06_rank_zset 300 0 byscore rev limit 1 2 withscores // 取300分到0分之间的排名
+	// zrange ex06_rank_zset -inf +inf byscore withscores 正序输出
+	// zrange ex06_rank_zset +inf -inf byscore rev WITHSCORES 逆序输出所有排名
+	// zrange ex06_rank_zset +inf -inf byscore rev limit 0 2 withscores 逆序分页输出排名
+	zRangeArgs := redis.ZRangeArgs{
+		Key:     ex06RankKey,
+		ByScore: true,
+		Rev:     true,
+		Start:   "-inf",
+		Stop:    "+inf",
+		Offset:  offset,
+		Count:   pageSize,
+	}
+	resList, err := RedisCli.ZRangeArgsWithScores(ctx, zRangeArgs).Result()
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("榜单(offset=%d, pageSize=%d)", offset, pageSize)
+	offNum := int(pageSize * offset)
+	for i, z := range resList {
+		rank := i + 1 + offNum
+		fmt.Printf("第%d名 %s\t%.2f\n", rank, z.Member, z.Score)
+	}
+}
+
+// 获取用户排名
+func ex06GetUserRankByName(ctx context.Context, name string) {
+	rank, err := RedisCli.ZRevRank(ctx, ex06RankKey, name).Result()
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("name=%s, rank=%d", name, rank)
+}
+
+// 获取用户分数信息
+func ex06GetUserScoreByName(ctx context.Context, name string) {
+	score, err := RedisCli.ZScore(ctx, ex06RankKey, name).Result()
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("name=%s, score=%.2f", name, score)
+}
+
+// ex06AddUserScore 增加用户分数
+func ex06AddUserScore(ctx context.Context, name string, score float64) {
+	num, err := RedisCli.ZIncrBy(ctx, ex06RankKey, score, name).Result()
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("name=%s, add_score=%.2f, score=%.2f", name, score, num)
+}
