60977a0f71e18a79be2e6f9a043bf6c4b19ae854
pjm <2556323541@qq.com>
2024-03-25 23:52:43 +0800

    update: example05

--- go-redis-example/example/ex05_hash_count.go
+++ go-redis-example/example/ex05_hash_count.go
@@ -13,9 +13,9 @@ const ex05UserCountKey = "ex05_user_count"
 // go run main.go Ex05 init 初始化用户计数值
 // go run main.go Ex05 get 1556564194374926  // 打印用户(1556564194374926)的所有计数值
 // go run main.go Ex05 incr_like 1556564194374926 // 点赞数+1
-// go run main.go Ex05 incr_collect 1556564194374926 // 点赞数+1
+// go run main.go Ex05 incr_collect 1556564194374926 // 收藏数+1
 // go run main.go Ex05 decr_like 1556564194374926 // 点赞数-1
-// go run main.go Ex05 decr_collect 1556564194374926 // 点赞数-1
+// go run main.go Ex05 decr_collect 1556564194374926 // 收藏数-1
 func Ex05(ctx context.Context, args []string) {
 	if len(args) <= 0 {
 		panic("args can't be empty")
@@ -30,30 +30,30 @@ func Ex05(ctx context.Context, args []string) {
 			panic(err)
 		}
 		Ex05GetUserCount(ctx, userID)
-	//case "incr_like":
-	//	userID, err := strconv.ParseInt(args[1], 10, 64)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	IncrByUserLike(ctx, userID)
-	//case "incr_collect":
-	//	userID, err := strconv.ParseInt(args[1], 10, 64)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	IncrByUserCollect(ctx, userID)
-	//case "decr_like":
-	//	userID, err := strconv.ParseInt(args[1], 10, 64)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	DecrByUserLike(ctx, userID)
-	//case "decr_collect":
-	//	userID, err := strconv.ParseInt(args[1], 10, 64)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	DecrByUserCollect(ctx, userID)
+	case "incr_like":
+		userID, err := strconv.ParseInt(args[1], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		IncrByUserLike(ctx, userID)
+	case "incr_collect":
+		userID, err := strconv.ParseInt(args[1], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		IncrByUserCollect(ctx, userID)
+	case "decr_like":
+		userID, err := strconv.ParseInt(args[1], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		DecrByUserLike(ctx, userID)
+	case "decr_collect":
+		userID, err := strconv.ParseInt(args[1], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		DecrByUserCollect(ctx, userID)
 	default:
 		panic("do not support now...")
 	}
@@ -106,4 +106,58 @@ func Ex05GetUserCount(ctx context.Context, userID int64) {
 	for k, v := range results {
 		fmt.Printf("%s: %s\n", k, v)
 	}
+}
+
+// IncrByUserLike 点赞数+1
+func IncrByUserLike(ctx context.Context, userID int64) {
+	incrByUserField(ctx, userID, "got_digg_count")
+}
+
+// IncrByUserCollect 收藏数+1
+func IncrByUserCollect(ctx context.Context, userID int64) {
+	incrByUserField(ctx, userID, "follow_collect_set_count")
+}
+
+// DecrByUserLike 点赞数-1
+func DecrByUserLike(ctx context.Context, userID int64) {
+	decrByUserField(ctx, userID, "got_digg_count")
+}
+
+// DecrByUserCollect 收藏数-1
+func DecrByUserCollect(ctx context.Context, userID int64) {
+	decrByUserField(ctx, userID, "follow_collect_set_count")
+}
+
+func incrByUserField(ctx context.Context, userID int64, field string) {
+	change(ctx, userID, field, 1)
+}
+
+func decrByUserField(ctx context.Context, userID int64, field string) {
+	change(ctx, userID, field, -1)
+}
+
+func change(ctx context.Context, userID int64, field string, delta int64) {
+	key := ex05GetUserCounterKey(userID)
+	before, err := RedisCli.HGet(ctx, key, field).Result()
+	if err != nil {
+		panic(err)
+	}
+	beforeInt, err := strconv.ParseInt(before, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	if beforeInt+delta < 0 {
+		fmt.Printf("禁止变更计数，计数变更后小于0. %d + (%d) = %d\n", beforeInt, delta, beforeInt+delta)
+		return
+	}
+	fmt.Printf("user[%d]: \n更新前\n%s = %s\n--------\n", userID, field, before)
+	if err = RedisCli.HIncrBy(ctx, key, field, delta).Err(); err != nil {
+		panic(err)
+	}
+	count, err := RedisCli.HGet(ctx, key, field).Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("user_id: %d\n更新后\n%s = %s\n--------\n", userID, field, count)
 }
